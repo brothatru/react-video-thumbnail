76a0286e1ff671d7c8afa4ac07c653ef52b8a49b
michael trieu <brothatru@gmail.com>
2018-04-18 20:12:27 -0700

    Updating module for npm

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * src/index.js
  * @author mike trieu
  */
-import VideoThumbnail from './components/VideoThumbnail';
+// import VideoThumbnail from './components/VideoThumbnail';
 
-export default VideoThumbnail;
+// export default VideoThumbnail;
+module.exports = require('../example/dist/main.js');
